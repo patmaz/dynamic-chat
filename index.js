c45721e5c6cee21d3cd8414eacb92ddb695db534
Patryk Mazurkiewicz <patrykiewicz@gmail.com>
2019-07-17 07:48:00 +0200

    /ping endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 const express = require('express');
 const http = require('http');
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
 
 const redisClient = require("redis").createClient();
 
@@ -11,6 +13,13 @@ const app = express();
 const server = http.createServer(app);
 
 const port = 3030;
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 20,
+});
+
+app.set('trust proxy', 1);
 
 //errors
 process.on('uncaughtException', (err) => {
@@ -23,6 +32,11 @@ app.use(express.static('public'));
 //modules
 secureChat(app, redisClient);
 chat(server, redisClient);
+
+// ping
+app.get('/ping', cors(), limiter, (req, res) => {
+  res.json({ data: 'pong' });
+});
 
 //404
 app.use((req, res, next) => {
